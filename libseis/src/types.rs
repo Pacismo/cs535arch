6ef4f7fa76599e0ac44aeade06c1f18c0cd1fdb1
Francisco Santana <30952311+Pacismo@users.noreply.github.com>
2024-02-28 08:04:48 -0500

    Signed types

--- libseis/src/types.rs
+++ libseis/src/types.rs
@@ -1,10 +1,18 @@
 /// One byte \[0, 255]
 pub type Byte = u8;
+/// One byte \[-128, 127]
+pub type SByte = i8;
 /// Two bytes \[0, 65535]
 pub type Short = u16;
+/// Two bytes \[-32768. 32767]
+pub type SShort = i16;
 /// Four bytes \[0, 4294967295]
 pub type Long = u32;
+/// Four bytes \[-2147483648, 2147483647]
+pub type SLong = i32;
 /// The width of an instruction (an alias for a [`Long`])
 pub type Word = Long;
+/// The width of an instruction (an alias for a [`SLong`])
+pub type SWord = SLong;
 /// Represents a register index.
 pub type Register = Byte;
