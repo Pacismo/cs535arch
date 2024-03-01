31e598529b756dc2810ccdafc94b0857f46ce894
Francisco Santana <francisco.santana2002@gmail.com>
2024-02-29 21:20:57 -0500

    Implementing assembler cli

---
+++ seis-asm/src/cli/mod.rs
@@ -0,0 +1,15 @@
+use clap::{Parser, ValueHint::FilePath};
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(version, about, long_about = None)]
+pub struct Command {
+    /// The files to assemble.
+    #[clap(num_args=1.., value_delimiter=' ', value_hint=FilePath)]
+    pub files: Vec<PathBuf>,
+    /// Where to store the output.
+    ///
+    /// a.out by default
+    #[clap(short='o', value_hint=FilePath)]
+    pub output: Option<PathBuf>,
+}
