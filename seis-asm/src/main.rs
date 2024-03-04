173b78041fa62680e04677395f21d84b7e9d3416
Francisco Santana <francisco.santana2002@gmail.com>
2024-03-04 16:43:25 -0500

    Printing out tokens differently, again.

--- seis-asm/src/main.rs
+++ seis-asm/src/main.rs
@@ -7,10 +7,9 @@ mod parse;
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = cli::Command::parse();
 
-    match parse::tokenize(&cli.files[0]).map_err(|e| e.to_string()) {
-        Ok(tokens) => println!("{tokens:#?}"),
-        Err(e) => println!("{e}"),
-    }
+    let tokens = parse::tokenize(&cli.files[0]).map_err(|e| e.to_string())?;
+
+    println!("{tokens:?}");
 
     Ok(())
 }
