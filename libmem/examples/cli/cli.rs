ddd68f623bc0240bbd1c108d6092f56c6d01c210
Francisco Santana <30952311+Pacismo@users.noreply.github.com>
2024-03-10 11:40:46 -0400

    No more ways, now the miss penalty and writethrough flag can be set

--- libmem/examples/cli/cli.rs
+++ libmem/examples/cli/cli.rs
@@ -14,8 +14,6 @@ fn ranged<const MIN: usize, const MAX: usize>(val: &str) -> Result<usize, String
 pub enum CacheMode {
     None,
     Associative {
-        #[arg(value_parser = ranged::<1, 16>)]
-        ways: usize,
         #[arg(value_parser = ranged::<0, 32>)]
         set_bits: usize,
         #[arg(value_parser = ranged::<2, 32>)]
@@ -25,8 +23,17 @@ pub enum CacheMode {
 
 #[derive(Parser)]
 pub struct Args {
+    /// How many pages to allocate
     #[arg(value_parser = ranged::<3, 65536>)]
     pub pages: usize,
+    /// What kind of cache to use
     #[command(subcommand)]
     pub mode: CacheMode,
+
+    /// The penalty for a miss, in clocks
+    #[arg(default_value_t = 100, value_parser = ranged::<1, { usize::MAX }>)]
+    pub miss_penalty: usize,
+    /// Whether to have writes go through the cache on miss
+    #[arg(short, long)]
+    pub writethrough: bool,
 }
