895b2d4b826b6bc1f101bbace2e806dbf5ef132a
Francisco Santana <francisco.santana2002@gmail.com>
2024-03-13 23:21:47 -0400

    JSON serialization

--- libpipe/src/lib.rs
+++ libpipe/src/lib.rs
@@ -4,8 +4,13 @@
 //! The expectation is that the processor will implement the
 //! fetch, decode, execute, memory, and writeback stages.
 
+mod registers;
+
 use libmem::module::MemoryModule;
+pub use registers::Registers;
+use serde_json::ser::CompactFormatter;
 use std::fmt::Debug;
+use std::io::Write;
 
 /// The result of clocking the pipeline.
 pub enum ClockResult {
@@ -23,10 +28,24 @@ pub enum ClockResult {
     Dry,
 }
 
+type JsonSerializer<'a> = &'a mut serde_json::Serializer<&'a mut dyn Write, CompactFormatter>;
+type JsonSerializerResult<'a> = Result<
+    <JsonSerializer<'a> as serde::Serializer>::Ok,
+    <JsonSerializer<'a> as serde::Serializer>::Error,
+>;
+
 /// Represents a processor pipeline. Clocking the processor will yield
 /// a [`ClockResult`].
 pub trait Pipeline: Debug {
+    /// Applies a clock on the pipeline
     fn clock(&mut self) -> ClockResult;
 
+    /// Gets the memory module in the pipeline
     fn memory_module(&self) -> &dyn MemoryModule;
+
+    /// Gets the registers in the pipeline
+    fn registers(&self) -> &Registers;
+
+    /// Serializes the stages of the pipeline into JSON data
+    fn serialize_stages_json(&self, jser: JsonSerializer) -> JsonSerializerResult;
 }
