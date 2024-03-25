4f0241afa0389a6e59b13de0fe7858ee89118283
Francisco Santana <francisco.santana2002@gmail.com>
2024-03-25 18:21:03 -0400

    Clock requirement set

--- libpipe/src/stages/execute/resolver/register_ops.rs
+++ libpipe/src/stages/execute/resolver/register_ops.rs
@@ -1,15 +1,31 @@
-use crate::regmap::RegMap;
 use super::Resolver;
-use libseis::instruction_set::RegisterOp;
+use crate::{regmap::RegMap, stages::execute::ExecuteResult};
+use libseis::instruction_set::{
+    register::{ReadOp, WriteOp},
+    RegisterOp,
+};
 
 impl Resolver for RegisterOp {
     #[inline]
-    fn execute(self, regvals: RegMap) -> crate::stages::execute::ExecuteResult {
+    fn execute(self, regvals: RegMap) -> ExecuteResult {
         todo!()
     }
 
     #[inline]
     fn clock_requirement(self) -> usize {
-        todo!()
+        match self {
+            RegisterOp::Lbr(r) | RegisterOp::Lsr(r) | RegisterOp::Llr(r) => match r {
+                ReadOp::Indirect { .. } | ReadOp::ZeroPage { .. } | ReadOp::StackOffset { .. } => 1,
+                _ => 2,
+            },
+            RegisterOp::Sbr(w) | RegisterOp::Ssr(w) | RegisterOp::Slr(w) => match w {
+                WriteOp::ZeroPage { .. }
+                | WriteOp::Indirect { .. }
+                | WriteOp::StackOffset { .. } => 1,
+                _ => 2,
+            },
+            RegisterOp::Push(regs) | RegisterOp::Pop(regs) => regs.count(),
+            RegisterOp::Tfr(_) | RegisterOp::Ldr(_) => 1,
+        }
     }
 }
