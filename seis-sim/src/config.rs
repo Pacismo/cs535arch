b7b3be738f7af2796eff667a377be477eff7933c
Francisco Santana <30952311+Pacismo@users.noreply.github.com>
2024-04-01 01:09:50 -0400

    Configuration through TOML now possible

--- seis-sim/src/config.rs
+++ seis-sim/src/config.rs
@@ -1,13 +1,12 @@
+use crate::PAGES;
 use libmem::{
     cache::{Associative, Cache, MultiAssociative, NullCache},
     memory::Memory,
     module::{MemoryModule, SingleLevel},
 };
-use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, error::Error};
 
-use crate::PAGES;
-
-#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub enum CacheConfiguration {
     Disabled,
     Associative {
@@ -26,15 +25,6 @@ impl CacheConfiguration {
                 offset_bits,
                 ways,
             } => {
-                assert!(
-                    offset_bits >= 2,
-                    "Must have at least two bits for byte offset"
-                );
-                assert!(
-                    set_bits + offset_bits <= 32,
-                    "set_bits + offset_bits must sum to at most 32"
-                );
-
                 if ways == 1 {
                     Box::new(Associative::new(offset_bits, set_bits))
                 } else {
@@ -43,42 +33,155 @@ impl CacheConfiguration {
             }
         }
     }
-}
 
-#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
-pub enum MemoryConfiguration {
-    SingleLevel {
-        instruction_cache: CacheConfiguration,
-        data_cache: CacheConfiguration,
+    pub fn from_toml(table: &toml::Table) -> Result<Self, Box<dyn Error>> {
+        match table
+            .get("mode")
+            .ok_or_else(|| "Key required: \"mode\"")?
+            .as_str()
+            .ok_or_else(|| "Expected string")?
+        {
+            "disabled" => Ok(Self::Disabled),
+            "associative" => {
+                let set_bits = table
+                    .get("set_bits")
+                    .ok_or_else(|| "Key required: \"set_bits\"")?
+                    .as_integer()
+                    .ok_or_else(|| "Expected integer")? as usize;
+                let offset_bits = table
+                    .get("offset_bits")
+                    .ok_or_else(|| "Key required: \"offset_bits\"")?
+                    .as_integer()
+                    .ok_or_else(|| "Expected integer")? as usize;
+                let ways = table
+                    .get("ways")
+                    .ok_or_else(|| "Key required: \"ways\"")?
+                    .as_integer()
+                    .ok_or_else(|| "Expected integer")? as usize;
 
-        miss_penalty: usize,
-        volatile_penalty: usize,
-        writethrough: bool,
-    },
-}
+                Ok(Self::Associative {
+                    set_bits,
+                    offset_bits,
+                    ways,
+                })
+            }
 
-impl MemoryConfiguration {
-    pub fn into_boxed_module(self) -> Box<dyn MemoryModule> {
-        match self {
-            MemoryConfiguration::SingleLevel {
-                instruction_cache,
-                data_cache,
-                miss_penalty,
-                volatile_penalty,
-                writethrough,
-            } => Box::new(SingleLevel::new(
-                data_cache.into_boxed_cache(),
-                instruction_cache.into_boxed_cache(),
-                Memory::new(PAGES),
-                miss_penalty,
-                volatile_penalty,
-                writethrough,
-            )),
+            mode => Err(format!("Unrecognized mode: {mode}").into()),
         }
+    }
+
+    pub fn to_toml(self) -> toml::Table {
+        let mut table = toml::Table::new();
+
+        match self {
+            CacheConfiguration::Disabled => {
+                table.insert("mode".to_string(), "disabled".into());
+            }
+            CacheConfiguration::Associative {
+                set_bits,
+                offset_bits,
+                ways,
+            } => {
+                table.insert("mode".to_string(), "associative".into());
+                table.insert("set_bits".to_string(), (set_bits as i64).into());
+                table.insert("offset_bits".to_string(), (offset_bits as i64).into());
+                table.insert("ways".to_string(), (ways as i64).into());
+            }
+        }
+
+        table
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default)]
 pub struct SimulationConfiguration {
-    pub memory_mode: MemoryConfiguration,
+    pub cache: HashMap<String, CacheConfiguration>,
+
+    pub miss_penalty: usize,
+    pub volatile_penalty: usize,
+    pub writethrough: bool,
+}
+
+impl SimulationConfiguration {
+    pub fn into_boxed_module(self) -> Box<dyn MemoryModule> {
+        let data_config = self
+            .cache
+            .get("data")
+            .expect("Expected a configuration for the data cache");
+        let instruction_config = self
+            .cache
+            .get("instruction")
+            .expect("Expected a configuration for the instruction cache");
+
+        Box::new(SingleLevel::new(
+            data_config.into_boxed_cache(),
+            instruction_config.into_boxed_cache(),
+            Memory::new(PAGES),
+            self.miss_penalty,
+            self.volatile_penalty,
+            self.writethrough,
+        ))
+    }
+
+    pub fn from_toml(table: &toml::Table) -> Result<Self, Box<dyn Error>> {
+        let mut result = SimulationConfiguration::default();
+
+        result.miss_penalty = table
+            .get("miss_penalty")
+            .ok_or_else(|| "Key required: \"miss_penalty\"")?
+            .as_integer()
+            .ok_or_else(|| "Expected integer")? as usize;
+        result.volatile_penalty = table
+            .get("volatile_penalty")
+            .ok_or_else(|| "Key required: \"volatile_penalty\"")?
+            .as_integer()
+            .ok_or_else(|| "Expected integer")? as usize;
+        result.writethrough = table
+            .get("writethrough")
+            .ok_or_else(|| "Key required: \"writethrough\"")?
+            .as_bool()
+            .ok_or_else(|| "Expected boolean")?;
+
+        result.cache = table
+            .get("cache")
+            .ok_or_else(|| "Key required: \"cache\"")?
+            .as_table()
+            .ok_or_else(|| "Expected table")?
+            .into_iter()
+            .map(|(key, value)| {
+                Ok((
+                    key.to_owned(),
+                    CacheConfiguration::from_toml(
+                        value.as_table().ok_or_else(|| "Expected table")?,
+                    )?,
+                ))
+            })
+            .collect::<Result<HashMap<String, CacheConfiguration>, Box<dyn Error>>>()?;
+
+        Ok(result)
+    }
+
+    pub fn to_toml(&self) -> toml::Table {
+        let mut table = toml::Table::new();
+
+        table.insert(
+            "miss_penalty".to_string(),
+            (self.miss_penalty as i64).into(),
+        );
+        table.insert(
+            "volatile_penalty".to_string(),
+            (self.volatile_penalty as i64).into(),
+        );
+        table.insert("writethrough".to_string(), self.writethrough.into());
+
+        let mut caches = toml::Table::new();
+
+        for (name, config) in &self.cache {
+            caches.insert(name.to_owned(), config.to_toml().into());
+        }
+
+        table.insert("cache".to_string(), caches.into());
+
+        table
+    }
 }
--- seis-sim/src/main.rs
+++ seis-sim/src/main.rs
@@ -1,8 +1,51 @@
 mod cli;
 mod config;
 
+use config::{CacheConfiguration, SimulationConfiguration};
+use toml::toml;
+
 const PAGES: usize = 64;
 
 fn main() {
-    println!("Hello, world!");
+    let config = SimulationConfiguration {
+        cache: [
+            ("data".to_string(), CacheConfiguration::Disabled),
+            (
+                "instruction".to_string(),
+                CacheConfiguration::Associative {
+                    set_bits: 2,
+                    offset_bits: 2,
+                    ways: 2,
+                },
+            ),
+        ]
+        .into(),
+
+        miss_penalty: 10,
+        volatile_penalty: 2,
+        writethrough: true,
+    };
+
+    let toml = config.to_toml();
+
+    println!("{toml}");
+
+    let table = toml! {
+        miss_penalty = 10
+        volatile_penalty = 2
+        writethrough = true
+
+        [cache.data]
+        mode = "disabled"
+
+        [cache.instruction]
+        mode = "associative"
+        set_bits = 2
+        offset_bits = 2
+        ways = 2
+    };
+
+    let config = SimulationConfiguration::from_toml(&table).unwrap();
+
+    println!("{config:#?}")
 }
