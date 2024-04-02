eec2cb2b44956120a54ca801f06609047a7913e4
Francisco Santana <30952311+Pacismo@users.noreply.github.com>
2024-04-02 11:55:25 -0400

    The way the example gets printed is by using a real configuration, to require that I actually maintain the datastructure

--- seis-sim/src/main.rs
+++ seis-sim/src/main.rs
@@ -3,14 +3,33 @@ mod config;
 
 use clap::Parser;
 use cli::{Cli, SimulatorConfig};
-use config::{CacheConfiguration, SimulationConfiguration};
-use std::{error::Error, path::PathBuf};
-use toml::toml;
+use config::{CacheConfiguration, PipelineMode, SimulationConfiguration};
+use libpipe::{Pipeline, Pipelined, Unpipelined};
+use libseis::{pages::PAGE_SIZE, types::Word};
+use std::{error::Error, fs::read, io::Read, path::PathBuf};
 
 /// However many pages of memory are supported by the simulator
 const PAGES: usize = 16;
 
 fn run_config(conf: PathBuf, bin: PathBuf) -> Result<(), Box<dyn Error>> {
+    let conf = SimulationConfiguration::from_toml_file(conf)?;
+    let mut pipeline = conf.into_boxed_pipeline();
+
+    let memory = pipeline.memory_module_mut().memory_mut();
+
+    let data = read(&bin)?;
+
+    if data.len() >= PAGES << 16 {
+        return Err(format!("File too long: {}", bin.display()).into());
+    }
+
+    for (page, data) in data.chunks(PAGE_SIZE).enumerate() {
+        memory.set_page((page << 16) as Word, data)
+    }
+
+    /// TODO:
+    println!("Run program");
+
     Ok(())
 }
 
@@ -18,24 +37,35 @@ fn main() {
     let cli = Cli::parse();
 
     if let Some(info) = cli.info {
-        if info.print_example_config {
-            println!(
-                "{}",
-                toml! {
-                    miss_penalty = 10
-                    volatile_penalty = 2
-                    writethrough = true
+        let example = SimulationConfiguration {
+            cache: [
+                ("instruction".into(), CacheConfiguration::Disabled),
+                (
+                    "data".into(),
+                    CacheConfiguration::Associative {
+                        set_bits: 2,
+                        offset_bits: 2,
+                        ways: 2,
+                    },
+                ),
+            ]
+            .into(),
+            miss_penalty: 100,
+            volatile_penalty: 20,
+            writethrough: false,
 
-                    [cache.data]
-                    mode = "disabled"
+            pipelining: PipelineMode::Enabled,
+        };
 
-                    [cache.instruction]
-                    mode = "associative"
-                    set_bits = 2
-                    offset_bits = 2
-                    ways = 2
-                }
-            );
+        if let Some(ex) = info.print_example_config {
+            if let Some(out) = ex {
+                std::fs::write(&out, example.to_toml().to_string()).expect(&format!(
+                    "Failed to write example output to {}",
+                    out.display()
+                ))
+            } else {
+                println!("{}", example.to_toml());
+            }
         }
     } else if let Some(SimulatorConfig {
         configuration,
