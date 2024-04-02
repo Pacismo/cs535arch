b388b2538768c3f1c2e3156e5ee40f3758226491
Francisco Santana <30952311+Pacismo@users.noreply.github.com>
2024-04-02 11:53:52 -0400

    Pipelining can now be enabled or disabled in the configuration

--- seis-sim/src/config.rs
+++ seis-sim/src/config.rs
@@ -2,9 +2,10 @@ use crate::PAGES;
 use libmem::{
     cache::{Associative, Cache, MultiAssociative, NullCache},
     memory::Memory,
-    module::{MemoryModule, SingleLevel},
+    module::SingleLevel,
 };
-use std::{collections::HashMap, error::Error, path::PathBuf};
+use libpipe::{Pipeline, Pipelined, Unpipelined};
+use std::{collections::HashMap, error::Error, fmt::Display, path::PathBuf, str::FromStr};
 
 #[derive(Debug, Clone, Copy)]
 pub enum CacheConfiguration {
@@ -93,6 +94,34 @@ impl CacheConfiguration {
     }
 }
 
+#[derive(Debug, Clone, Copy, Default)]
+pub enum PipelineMode {
+    #[default]
+    Disabled,
+    Enabled,
+}
+
+impl Display for PipelineMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PipelineMode::Disabled => write!(f, "disabled"),
+            PipelineMode::Enabled => write!(f, "enabled"),
+        }
+    }
+}
+
+impl FromStr for PipelineMode {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "disabled" => Ok(Self::Disabled),
+            "enabled" => Ok(Self::Enabled),
+            _ => Err(format!("{s} is not a recognized pipelining mode")),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 pub struct SimulationConfiguration {
     pub cache: HashMap<String, CacheConfiguration>,
@@ -100,10 +129,12 @@ pub struct SimulationConfiguration {
     pub miss_penalty: usize,
     pub volatile_penalty: usize,
     pub writethrough: bool,
+
+    pub pipelining: PipelineMode,
 }
 
 impl SimulationConfiguration {
-    pub fn into_boxed_module(self) -> Box<dyn MemoryModule> {
+    pub fn into_boxed_pipeline(self) -> Box<dyn Pipeline> {
         let data_config = self
             .cache
             .get("data")
@@ -113,14 +144,19 @@ impl SimulationConfiguration {
             .get("instruction")
             .expect("Expected a configuration for the instruction cache");
 
-        Box::new(SingleLevel::new(
+        let mem = SingleLevel::new(
             data_config.into_boxed_cache(),
             instruction_config.into_boxed_cache(),
             Memory::new(PAGES),
             self.miss_penalty,
             self.volatile_penalty,
             self.writethrough,
-        ))
+        );
+
+        match self.pipelining {
+            PipelineMode::Disabled => Box::new(Unpipelined::new(Box::new(mem))),
+            PipelineMode::Enabled => Box::new(Pipelined::new(Box::new(mem))),
+        }
     }
 
     pub fn from_toml(table: &toml::Table) -> Result<Self, Box<dyn Error>> {
@@ -141,6 +177,21 @@ impl SimulationConfiguration {
             .ok_or_else(|| "Key required: \"writethrough\"")?
             .as_bool()
             .ok_or_else(|| "Expected boolean")?;
+        result.pipelining = if let Some(value) = table.get("pipelining") {
+            if let Some(str) = value.as_str() {
+                str.parse()?
+            } else if let Some(mode) = value.as_bool() {
+                if mode {
+                    PipelineMode::Enabled
+                } else {
+                    PipelineMode::Disabled
+                }
+            } else {
+                return Err("Key \"pipelining\" must be either a string or a boolean".into());
+            }
+        } else {
+            PipelineMode::default()
+        };
 
         result.cache = table
             .get("cache")
@@ -173,6 +224,14 @@ impl SimulationConfiguration {
             (self.volatile_penalty as i64).into(),
         );
         table.insert("writethrough".to_string(), self.writethrough.into());
+        table.insert(
+            "pipelining".to_string(),
+            match self.pipelining {
+                PipelineMode::Disabled => false.into(),
+                PipelineMode::Enabled => true.into(),
+                // _ => self.pipelining.to_string().into(),
+            },
+        );
 
         let mut caches = toml::Table::new();
 
@@ -188,6 +247,6 @@ impl SimulationConfiguration {
     pub fn from_toml_file(file: PathBuf) -> Result<Self, Box<dyn Error>> {
         let fdata = std::fs::read_to_string(file)?;
         let table = toml::from_str(&fdata)?;
-        Self::from_toml(table)
+        Self::from_toml(&table)
     }
 }
