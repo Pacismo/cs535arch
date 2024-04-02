de8f73fff55315e3c48b691d1fa385d77266ccc2
Francisco Santana <30952311+Pacismo@users.noreply.github.com>
2024-04-01 21:39:26 -0400

    Disassembly code

---
+++ seis-disasm/src/main.rs
@@ -0,0 +1,83 @@
+use clap::{Parser, ValueHint::FilePath};
+use libseis::{
+    instruction_set::{decode, Instruction},
+    types::Word,
+};
+use std::{fs::read, path::PathBuf};
+
+#[derive(Parser, Debug, Clone)]
+pub struct Cli {
+    #[arg(value_hint = FilePath)]
+    pub file: PathBuf,
+
+    #[arg(short)]
+    pub binary: bool,
+}
+
+fn main() {
+    let Cli { file, binary } = Cli::parse();
+
+    let content = read(file).expect("Failed to read file");
+
+    if binary {
+        println!(
+            "{:<10} | {:>8} | {:>8} | {:>8} | {:>8} | {}",
+            "Address", "+0", "+1", "+2", "+3", "Instruction"
+        );
+        println!("-----------|----------|----------|----------|----------|------------------");
+    } else {
+        println!(
+            "{:<10} | {:>2} | {:>2} | {:>2} | {:>2} | {}",
+            "Address", "+0", "+1", "+2", "+3", "Instruction"
+        );
+        println!("-----------|----|----|----|----|------------------");
+    }
+
+    for (i, word) in content.chunks(4).enumerate() {
+        if word.len() == 4 {
+            let instruction = match decode::<Instruction>(Word::from_be_bytes([
+                word[0], word[1], word[2], word[3],
+            ])) {
+                Ok(instruction) => instruction.to_string(),
+                Err(_) => "unknown".to_string(),
+            };
+
+            if binary {
+                println!(
+                    "{:#010X} | {:08b} | {:08b} | {:08b} | {:08b} | {instruction}",
+                    i * 4,
+                    word[0],
+                    word[1],
+                    word[2],
+                    word[3]
+                );
+            } else {
+                println!(
+                    "{:#010X} | {:02X} | {:02X} | {:02X} | {:02X} | {instruction}",
+                    i * 4,
+                    word[0],
+                    word[1],
+                    word[2],
+                    word[3]
+                );
+            }
+        } else {
+            print!("{i:#010X}");
+            for byte in word {
+                if binary {
+                    print!(" | {byte:08b}");
+                } else {
+                    print!(" | {byte:02X}");
+                }
+            }
+            for _ in word.len()..4 {
+                if binary {
+                    print!(" |         ");
+                } else {
+                    print!(" |   ");
+                }
+            }
+            println!();
+        }
+    }
+}
