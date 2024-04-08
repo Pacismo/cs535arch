90419a8852b5754274acdb23210d6f1b6c012ed8
Francisco Santana <30952311+Pacismo@users.noreply.github.com>
2024-04-07 21:18:28 -0400

    Dynamically adding memoryview columns

--- gui/Controls/MemoryView.xaml.cs
+++ gui/Controls/MemoryView.xaml.cs
@@ -20,15 +20,13 @@ namespace gui.Controls
         public string Address { get; set; }
         public string[] Byte { get; set; }
 
-        public MemoryViewRow(uint address, byte[] bytes, bool binary = false)
+        public MemoryViewRow(uint address, byte[] bytes)
         {
             Address = $"0x{address:X8}";
             Byte = new string[bytes.Length];
 
             for (int i = 0; i < bytes.Length; ++i)
-            {
-                Byte[i] = binary ? $"{bytes[i]:b8}" : $"{bytes[i]:X2}";
-            }
+                Byte[i] = $"{bytes[i]:X2}";
         }
     }
 
@@ -37,12 +35,14 @@ namespace gui.Controls
     /// </summary>
     public partial class MemoryView : UserControl
     {
+        const uint COLUMNS = 32;
+
         public MemoryView()
         {
             InitializeComponent();
         }
 
-        public void UpdateData(Data.Page page, uint page_id, bool binary)
+        public void UpdateData(Data.Page page, uint page_id)
         {
             Data.Items.Clear();
             if (page.data == null)
@@ -50,18 +50,23 @@ namespace gui.Controls
             else
             {
                 Data.IsEnabled = true;
-                for (uint i = 0; i < page.data.Length; i += 16)
+                for (uint i = 0; i < page.data.Length; i += COLUMNS)
                 {
-                    byte[] bytes = new byte[16];
-                    for (uint j = 0; j < 16; j++)
+                    byte[] bytes = new byte[COLUMNS];
+                    for (uint j = 0; j < COLUMNS; j++)
                         bytes[j] = page.data[i + j];
 
-                    Data.Items.Add(new MemoryViewRow((page_id << 16) | i, bytes, binary));
+                    Data.Items.Add(new MemoryViewRow((page_id << 16) | i, bytes));
                 }
-
-                foreach (var column in Data.Columns)
-                    column.Width = DataGridLength.SizeToCells;
             }
+        }
+
+        private void Data_Loaded(object sender, RoutedEventArgs e)
+        {
+            Data.Columns.Clear();
+            Data.Columns.Add(new DataGridTextColumn() { Binding = new Binding("Address"), Header = "Address", Width = DataGridLength.Auto });
+            for (uint i = 0; i < COLUMNS; ++i)
+                Data.Columns.Add(new DataGridTextColumn() { Binding = new Binding($"Byte[{i}]"), Header = $"{i:X2}", Width = DataGridLength.Auto });
         }
     }
 }
