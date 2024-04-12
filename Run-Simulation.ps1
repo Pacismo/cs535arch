c42156d49cc50fcacf2edc644612671bc927539a
Francisco Santana <francisco.santana2002@gmail.com>
2024-04-12 18:42:24 -0400

    Parameters

--- Run-Simulation.ps1
+++ Run-Simulation.ps1
@@ -1,4 +1,4 @@
-$benchmark = "matrix"
+$benchmark = $args[0]
 
 $configuration = "./benchmarks/$benchmark.toml"
 $codefile = "./benchmarks/$benchmark.asm"
@@ -7,6 +7,8 @@ $binfile = "./benchmarks/$benchmark.bin"
 $cargo_install_dir = "./gui/bin/Release/net8.0-windows/"
 
 cargo run --bin seis-asm -- $codefile -o $binfile
+
+
 
 cargo install --path ./seis-sim --root ./$cargo_install_dir
 cargo install --path ./seis-asm --root ./$cargo_install_dir
