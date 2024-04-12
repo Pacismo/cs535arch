41787ffd29bad2e808e54f6e0c402db45b672ad8
Francisco Santana <francisco.santana2002@gmail.com>
2024-04-12 12:40:15 -0400

    Writeback for the cache

--- libmem/src/module/mod.rs
+++ libmem/src/module/mod.rs
@@ -113,6 +113,9 @@ pub trait MemoryModule: Debug {
     /// Gets a hashmap containing mutable references to the caches in the memory module
     fn caches_mut<'a>(&'a mut self) -> HashMap<&'static str, &'a mut dyn Cache>;
 
+    /// Writes the cache back to memory. Returns [`Status::Idle`] on success.
+    fn immediate_writeback(&mut self) -> Status;
+
     /// Get the state of the cache structures
     ///
     /// Provides the names of the caches as well
